c7156b9ec2b6ebe908a57af928713ab88ef9dd8b
Diego Moreano <dfmoreano@gmail.com>
2023-11-30 00:20:31 -0500

    Update installPostgreSQL.sh

--- installPostgreSQL.sh
+++ installPostgreSQL.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Create the file repository configuration:
-#sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Import the repository signing key:
-#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Update the package lists:
-#sudo apt-get update
+sudo apt-get update
 
 # Install the latest version of PostgreSQL.
 # Install PostgreSQL

902d27b25b30fc86b496570cf0e820ec07e96f27
Diego Moreano <dfmoreano@gmail.com>
2024-03-16 12:16:05 -0600

    Update GeekInstaller.sh
    jammy

--- GeekInstaller.sh
+++ GeekInstaller.sh
@@ -263,8 +263,9 @@ function mongodb_install() {
 			#curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o -y /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 			curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 			#echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-			echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-			sleep 2s			
+			#echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+			echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+   			sleep 2s
 			clear
 			echo -e "\nUpdating system"
 			sudo apt-get update -y
